2c8658fddc039cf73ec33e91f41df63455e5d22c
Iskra <7evenx@mail.ru>
2021-04-29 08:49:20 +0300

    v1

---
+++ BrokerageApp/Pages/AutorizationPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrokerageApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationPage.xaml
+    /// </summary>
+    public partial class AutorizationPage : Page
+    {
+        public AutorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBut_Click(object sender, RoutedEventArgs e)
+        {
+
+            var clients = MainWindow.DB.Client.FirstOrDefault(c => c.Login == LoginTB.Text && c.Password == PasswordPB.Password);
+            var manager = MainWindow.DB.Manager.FirstOrDefault(m => m.Login == LoginTB.Text && m.Password == PasswordPB.Password);
+            if (clients != null)
+            {
+                NavigationService.Navigate(new Pages.ClientPage(clients));
+
+            }
+            if (manager != null)
+            {
+                NavigationService.Navigate(new ManagerPage());
+            }
+            
+        }
+
+        private void RegBut_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ClientRegPage());
+        }
+    }
+}
---
+++ BrokerageApp/Pages/ClientPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using BrokerageApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrokerageApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        Client contextClient;
+        public ClientPage(Client client)
+        {
+            InitializeComponent();
+            contextClient = client;
+            ClintListView.ItemsSource = MainWindow.DB.Client.Where(c => c.Id == contextClient.Id).ToList();
+            this.DataContext = contextClient;
+        }
+    }
+}
